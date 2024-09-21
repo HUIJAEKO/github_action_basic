2a4d07930a99d58f4fce44053f5ee65e3cfec6ed
HUIJAEKO <xrhgmlwox@naver.com>
2024-09-21 15:05:30 +0900

    first commit

---
+++ src/test/java/example/githubactionbasic/GithubActionBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package example.githubactionbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
